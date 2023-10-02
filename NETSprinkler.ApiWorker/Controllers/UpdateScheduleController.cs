cbc055b6fef892efb1f6651eebc1892425703239
nickyeichmann <enicky1980@outlook.com>
2023-10-02 19:33:29 +0200

    added extra controller method to test ports

--- NETSprinkler.ApiWorker/Controllers/UpdateScheduleController.cs
+++ NETSprinkler.ApiWorker/Controllers/UpdateScheduleController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using NETSprinkler.ApiWorker.Business.Services.Scheduler;
+using NETSprinkler.ApiWorker.Business.Services.Sprinkler;
 
 namespace NETSprinkler.ApiWorker.Controllers;
 
@@ -8,17 +9,35 @@ public class UpdateScheduleController: Controller
 {
     private readonly ILogger<UpdateScheduleController> _logger;
     private readonly ISchedulerService _schedulerService;
+    private readonly ISprinklerService _sprinklerService;
 
     public UpdateScheduleController(ILogger<UpdateScheduleController> logger
-        ,ISchedulerService schedulerService)
+        ,ISchedulerService schedulerService,
+        ISprinklerService sprinklerService)
     {
         _logger = logger;
         _schedulerService = schedulerService;
+        _sprinklerService = sprinklerService;
     }
 
     [HttpPost("CreateSchedule")]
     public async Task CreateSchedule(CancellationToken cancellationToken, [FromBody] int id)
     {
         await _schedulerService.CreateSchedule(id);
+    }
+
+    [HttpPost("test")]
+    public async Task Test(CancellationToken token, int id, int status)
+    {
+        switch (status)
+        {
+            case 0:
+                await _sprinklerService.StartAsync(id);
+                break;
+            case 1:
+                await _sprinklerService.StopAsync(id);
+                break;
+        }
+        
     }
 }
